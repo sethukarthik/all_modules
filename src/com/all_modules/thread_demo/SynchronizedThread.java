c3d7fd158036b0e33a9e12f3fc59a50747287195
Sethu Naga Karthik M <m.sethukarthik@gmail.com>
2021-07-25 12:33:48 +0530

    Synchronized Thread

---
+++ src/com/all_modules/thread_demo/SynchronizedThread.java
@@ -0,0 +1,55 @@
+package com.all_modules.thread_demo;
+
+public class SynchronizedThread extends Thread {
+	public int count = 0;
+	
+	//When a method marked as synchronized then a single thread able to access at a time. 
+	public synchronized void increment() {
+		count++;
+	}
+	
+	//Similarly we can define a specific line be synchronized
+//	public void increment() {
+//		synchronized (this) { // This is also called synchronized blog
+//			count++;
+//		}
+//	}
+	
+	public void syncThread() {
+		Thread thread_1 = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				for (int i = 0; i < 10000; i++) {
+					increment();
+				}
+				System.out.println(count);
+			}
+		});
+		
+		Thread thread_2 = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				for (int i = 0; i < 10000; i++) {
+					increment();
+				}
+				System.out.println(count);
+			}
+		});
+		thread_1.start();
+		thread_2.start();
+		
+		try {
+			thread_1.join();
+			thread_2.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		SynchronizedThread syThread = new SynchronizedThread();
+		syThread.syncThread();
+	}
+}
